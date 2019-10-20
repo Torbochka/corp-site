316fa1db30685bd1093fc1b30975d41d3833bf32
Alexander Tobek <sasha.torbek@yandex.ru>
2019-10-21 02:29:14 +0300

    add template method user delete

--- server/api/users.js
+++ server/api/users.js
@@ -2,4 +2,16 @@ const Router = require("koa-router");
 const router = new Router();
 const ENGINE = global.ENGINE;
 
+router.delete("/users/:id", async ctx => {
+  try {
+    ctx.body = await ENGINE.emit("deleteUser", ctx);
+  } catch (error) {
+    ctx.flash("error", {
+      message: error.message,
+      status: error.status || 400
+    });
+    ctx.redirect("/error");
+  }
+});
+
 module.exports = router.routes();
